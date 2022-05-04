f5ed3ab0e3bec77457eacc777328d67de3ee410b
J Lee <jay.j.lee.1@gmail.com>
2022-05-04 18:46:41 -0400

    seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Item.create(
+    title: "Reece the Dog",
+    slug: "Dog",
+    description: "The best dog around"
+    image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.k9web.com%2Fbreeds%2Faustralian-shepherd-lab-mix%2F&psig=AOvVaw2zAwAjfrcWP-xNPvLOMqj7&ust=1651789429485000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCKjZu6XxxvcCFQAAAAAdAAAAABAD",
+    favorites_count: 257,
+    user_id: 1
+)
