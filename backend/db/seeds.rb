a2430cc989aa4c8ae278e2eaab7e9ffbbe8bdad7
J Lee <jay.j.lee.1@gmail.com>
2022-05-10 11:46:59 -0400

    user seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -13,3 +13,9 @@ Item.create(
     favorites_count: 257,
     user_id: 1
 )
+User.create(
+    email: "reece@reece.com",
+    username: "reecethedog",
+    image: "https://media.istockphoto.com/photos/crazy-looking-black-and-white-border-collie-dog-say-looking-intently-picture-id1213516345?k=20&m=1213516345&s=612x612&w=0&h=_XUSwcrXe5HjI2QEby0ex6Tl1fB_YJUzUU8o2cUt0YA=",
+    bio: "The best dog around"
+)
