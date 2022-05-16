e023bdbbb22fca3a548b5f24f808fbc50625597c
J Lee <jay.j.lee.1@gmail.com>
2022-05-16 19:49:00 -0400

    new seed no faker

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,24 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
+
 100.times do
 
     Item.create(
-    title: Faker::Kpop.i_groups ,
-    slug: Faker::Kpop.ii_groups,
-    description: Faker::Kpop.iii_groups,
-    image: Faker::Kpop.girl_groups,
+    title: "rand(1..100)" ,
+    slug: "rand(1..100)",
+    description: "rand(1..100)",
+    image: "rand(1..100)",
     favorites_count: rand(1..100),
     user_id: rand(1..100)
     )
     User.create(
-    email: Faker::Internet.email,
-    username: Faker::Name.name,
-    image: Faker::LoremPixel.image,
-    bio: Faker::Movies::Ghostbusters.quote
+    email: "blank@blank.com",
+    username: "Faker::Name.name",
+    image: "Faker::LoremPixel.image",
+    bio: "Faker::Movies::Ghostbusters.quote"
     )
     Comment.create(
-        body: Faker::Quote.yoda
+        body: "Faker::Quote.yoda"
     )
 end
